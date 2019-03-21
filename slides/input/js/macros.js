4681c4c639ef5e508592fa74cb1967dfa275a7a0
Janus Valberg-Madsen <janus@math.aau.dk>
2019-03-21 17:00:55 +0100

    Add some custom css and js

---
+++ slides/input/js/macros.js
@@ -0,0 +1,4 @@
+remark.macros.center = function (scale) {
+    var url = this;
+    return '<img src="' + url + '"width="' + scale + '" style="display:block; margin-left:auto; margin-right:auto;" />';
+};
